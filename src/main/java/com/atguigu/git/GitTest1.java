80c997a1bb6fb92ee88b0571e63e4b9b267abd07
wsy <1012865770@qq.com>
2022-08-30 10:35:58 +0800

    frist commit

---
+++ src/main/java/com/atguigu/git/GitTest1.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 我亦无他唯手熟尔
+ * @versoin 1.0
+ */
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
