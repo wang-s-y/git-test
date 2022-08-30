0173c15b3ec5303dbc94d988b4e59e3e584662fb
wsy <1012865770@qq.com>
2022-08-30 12:35:10 +0800

    hot-fix测试合并冲突

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hot-fix hello git");
+        System.out.println("hot-fix测试合并冲突");
     }
 }
