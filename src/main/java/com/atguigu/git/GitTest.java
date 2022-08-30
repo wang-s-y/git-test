9ef808216578d0c6e21395da59858c5f335bcc65
wang-s-y <60687410+wang-s-y@users.noreply.github.com>
2022-08-30 16:46:21 +0800

    PULL到本地库

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("hot-fix测试合并冲突");
         System.out.println("hot-fix测试合并冲突2");
         System.out.println("push到远程库当中");
+        System.out.println("pull到本地库当中");
 
     }
 }
